5be37de9467fe9c888f3517e7723ea1ae7a7e18e
Eric Ervin <eervin@blockforcecapital.com>
2023-03-22 21:31:54 -0700

    made changes to the app going to move computers

--- prediction-app/app.py
+++ prediction-app/app.py
@@ -6,6 +6,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle, os
+import numpy as np
 from data import load_data_with_features
 from predict import make_prediction, calculate_and_print_results, print_forecast
 import logging
@@ -40,46 +41,55 @@ model_files = [os.path.join(model_folder_path, f) for f in model_files if int(f.
 logging.info(f"Found {len(model_files)} model files. They are {model_files}")
 
 # Define the options for the dropdown
-options = [1, 5, 7, 14, 30]
+options = [1,2,3,4,5,6]
 
-# Add a dropdown to select the number of hours to display
+# Add a dropdown to select the number of days to display
 num_hours = st.selectbox('Select the number of hours to display', options)
 
 # Update the plot_filter variable based on the selected option
 plot_filter = num_hours * 60
 
 # Plot the data using the updated plot_filter variable
+# Plot the data using the updated plot_filter variable
 fig, ax = plt.subplots()
-ax.plot(data.index[-plot_filter:], data['Close'].iloc[-plot_filter:], label='Actual Price') # last num_hours hours
+ax.plot(data.index[-plot_filter:], data['Close'].iloc[-plot_filter:], label='Actual Price') # last num_days days
 
 # Get the predictions
-accuracy_df = pd.DataFrame(columns=["Forecast Period", "Accuracy", "MSE", "RMSE", "Precision Accuracy", "Direction Accuracy", "R-squared"])
+results_df = pd.DataFrame(columns=["Forecast Period", "y_val", "y_pred"])
+
+# Get the predictions
+accuracy_df = pd.DataFrame(columns=["Forecast Period", "Price Change", "Accuracy", "Direction Accuracy"])
+# Define the axis for the predictions plot
+predictions_fig, predictions_ax = plt.subplots()
+
 for i, forecast_period in enumerate(forecast_periods):
     logging.info(f"Loading model for the {i}'th model with a forecast period of {forecast_period} minutes...")
     logging.info(f"Looking for a model file with the name {model_files[0]}")
-    st.write(f"Predicted price change in {forecast_period} minutes:")
     if model_files[i]:
-        y_pred, y_val, price_change = make_prediction(data, forecast_period, model_files[i])
-        st.write(f"The current price of bitcoin is {current_price}. Predicted price change in the next {forecast_period} minutes is: ${price_change:.2f}")
-        fig, ax = plt.subplots()
-        ax.plot(data.index[-plot_filter:], data['Close'].iloc[-plot_filter:], label='Actual Price') # last num_hours hours
-        ax.plot(data.index[-1:], data['Close'].iloc[-1:], marker='o', markersize=5, label='Current Price')
-        ax.plot(data.index[-1:] + pd.Timedelta(minutes=forecast_period), price_change + current_price, marker='*', markersize=10, label='Predicted Price')
-        ax.set_xlabel('Time')
-        ax.set_ylabel('Price ($)')
-        ax.legend()
-        st.pyplot(fig)
-        results = calculate_and_print_results(y_val=y_val, y_pred=y_pred, threshold=0.001, forecast_period=forecast_period)
-        accuracy_df = pd.concat([accuracy_df, pd.DataFrame({"Forecast Period": f"{forecast_period} minutes", "Accuracy": results['accuracy'], "MSE": results['mse'], "RMSE": results['rmse'], "Precision Accuracy": results['accuracy'], "Direction Accuracy": results['direction_accuracy'], "R-squared": results['r2']}, index=[0])])
-        st.write("Prediction statistics:")
-        st.table(pd.DataFrame.from_dict(results, orient='index').T)
-    else:
-        st.write("Model not available.")
+        # Plot the predictions on the same figure
 
-# Show historical accuracy and forecasts        
+        y_pred, y_val, price_change = make_prediction(data, forecast_period, model_files[i])
+        forecast_price = (y_pred[-1] * data['Close'].iloc[-1]) + (data['Close'].iloc[-1])
+        results = calculate_and_print_results(y_val=y_val, y_pred=y_pred, threshold=0.001, forecast_period=forecast_period)
+        # results_df = results_df.append({"Forecast Period": f"{forecast_period} minutes", "y_val": y_val, "y_pred": y_pred}, ignore_index=True)
+        accuracy_df = accuracy_df.append({"Forecast Period": f"{forecast_period} minutes", 
+                                           "Price Change": f"${price_change:.2f}", 
+                                           "Accuracy": f"{results['accuracy']:.4f}%", 
+                                           "Direction Accuracy": f"{results['direction_accuracy']:.4f}%"}
+                                          , ignore_index=True)
+        # Plot the predicted values on the same figure
+        predictions_ax.plot(forecast_price, label=f"{forecast_period} minutes")
+
+        time_index = data.index[-1] + pd.Timedelta(minutes=forecast_period)
+        ax.scatter(time_index, forecast_price, marker='*', s=100, label=f"{forecast_period} minutes")
+
+# Show the actual and predicted values on the same plot
+ax.set_title('Actual and Predicted Prices')
+ax.set_xlabel('Time')
+ax.set_ylabel('Price (USD)')
+ax.legend()
+st.pyplot(fig)
+
 st.write("Historical accuracy:")
 st.write(accuracy_df)
 st.bar_chart(accuracy_df.set_index("Forecast Period"))
-
-
-
